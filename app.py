66b297283ab2dec64d38c283863c7b6f6cc7c46b
keisermanbeheer-maker <keiserman.beheer@gmail.com>
2026-01-15 12:35:25 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import json
 from email.message import EmailMessage
+import os
 import smtplib
 from datetime import datetime
 
@@ -59,4 +60,5 @@ return f"Reminders sent to {count} tenants."
 
 
 if __name__ == '__main__':
-app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
